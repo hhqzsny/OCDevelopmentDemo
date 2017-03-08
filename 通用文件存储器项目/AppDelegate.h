e2852353c8eda7a341e365e65668ae943b2f904e
黄华强 <1030063455@qq.com>
2017-03-08 09:33:38 +0800

    Initial Commit

---
+++ 通用文件存储器项目/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  通用文件存储器项目
+//
+//  Created by 黄华强 on 2017/3/8.
+//  Copyright © 2017年 黄华强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 通用文件存储器项目/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  通用文件存储器项目
+//
+//  Created by 黄华强 on 2017/3/8.
+//  Copyright © 2017年 黄华强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
